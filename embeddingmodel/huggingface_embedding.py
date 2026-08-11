93b63c6c2897a8ea98bd91a9712cd46717aed1d1
AliAnsari21 <aliaftab2106@gmail.com>
2026-08-11 17:19:29 +0530

    3: Embeddingmodel(Huggingface) with query

--- embeddingmodel/huggingface_embedding.py
+++ embeddingmodel/huggingface_embedding.py
@@ -3,5 +3,5 @@ from dotenv import load_dotenv
 load_dotenv()
 embedding=HuggingFaceEmbeddings(model="sentence-transformers/all-MiniLM-L6-v2")
 text="Delhi is capital of India"
-result=embedding.embed_query(text)
+result=embedding.embed_query(text)#for emedding query
 print(str(result))
